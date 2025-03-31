9d8809c949269e4abf90aabc39fcd9a96a84d499
GabrielValerianogit config --global user.name GabrielValeriano <valerianog1205@gmail.com>
2025-03-31 12:04:01 -0300

    Agregamos otra ruta

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/necesito/papel")
+def papel():
+    return "<p>Me olvide el papel del baño Ayuda</p>"
+
+@app.route("/necesito/salir")
+def salir():
+    return "<p>Me olvide la llave del baño Ayuda</p>"
