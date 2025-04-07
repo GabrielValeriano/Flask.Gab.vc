85e8f1a61ecc56136ec8ffa332c455945d096c16
GabrielValeriano <evabri2008cue@gmail.com>
2025-04-07 12:02:48 -0300

    Agregamos una ruta principal con links usando url_for

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
+@app.route("/")
+def Principal():
+    url_nota = url_for("notas")
+    url_Bpapel = url_for("papel")
+    url_llave = url_for("salir")
+
+    return """
+    <a href="{url_nota}">nota</a>
+    <br>
+    <a href="{url_Bpapel}">papel</a>
+    <br>
+    <a href="{url_llave}">salir</a>
+    """
+
 @app.route("/necesito/papel")
 def papel():
-    return "<p>Me olvide el papel del baño Ayuda</p>"
+    return """<p>Me olvide el papel del baño Ayuda</p>
+    <br>
+    <a href=">necesito/papel">papel</a>
+    <a href=">papel">papel 2</a>
+    """
 
 @app.route("/necesito/salir")
 def salir():
-    return """
-    <p>Me olvide la llave del baño Ayuda</p>
-    <a href="/static/PRUEBA.html">pagina</a>
-    """
+    return "<p>Me olvide la llave del baño Ayuda</p>"
 
 @app.route("/nota/DiseñoDeSoftware/<string:nombre>")
 def notas(nombre):
