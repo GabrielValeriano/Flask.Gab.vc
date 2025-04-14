0c03868c2b8fd2184c87ffdffa2456f7d99f2723
GabrielValeriano <valerianog1205@gmail.com>
2025-04-14 12:04:14 -0300

    Modificamos el registro de usuario con la utilizacion de rutas con argumentos

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 from flask import Flask
 from flask import Flask, url_for
+import sqlite3
 
 app = Flask(__name__)
 
+def dict_factory(cursor, row):
+   """Arma un diccionario con los valores de la fila."""
+   fields = [column[0] for column in cursor.description]
+   return {key: value for key, value in zip(fields, row)}
+
+def AbrirConexion():
+    global db
+    db = sqlite3.connect("instance/datos.sqlite")
+    db.row_factory = dict_factory
+
+def CerrarConexion():
+    global db 
+    db.close()
+    db = None
+
+@app.route("/test/DB")
+def testDB():
+    AbrirConexion()
+    cursor  = db.cursor()
+    cursor.execute("SELECT COUNT(*) AS cant FROM usuarios; ")
+    res = cursor.fetchone()
+    registros = res["cant"]
+    CerrarConexion()
+    return f"Hay {registros} registros en la tabla usuarios"
+
 @app.route("/")
 def Principal():
-    url_nota = url_for("notas")
+    url_nota = url_for("notas", nombre = "Gabriel")
     url_Bpapel = url_for("papel")
     url_llave = url_for("salir")
 
-    return """
+    return F"""
     <a href="{url_nota}">nota</a>
     <br>
     <a href="{url_Bpapel}">papel</a>
@@ -17,19 +43,52 @@ def Principal():
     <a href="{url_llave}">salir</a>
     """
 
+@app.route("/crearUsuario/<string:nombre>/<string:email>")
+def testCrear(nombre,email):
+    AbrirConexion()
+    cursor = db.cursor()
+    consulta = "INSERT INTO usuarios(usuario, email) VALUES (?, ?);"
+    cursor.execute(consulta, (nombre, email))
+    db.commit()
+    CerrarConexion()
+    return f"Registro agregado ({nombre})"
+
+@app.route("/crearUsuario/<string:nombre>/<string:email>")
+def testCrear(nombre,email):
+    AbrirConexion()
+    cursor = db.cursor()
+    consulta = "INSERT INTO usuarios(usuario, email) VALUES (?, ?);"
+    cursor.execute(consulta, (nombre, email))
+    db.commit()
+    CerrarConexion()
+    return f"Registro agregado ({nombre})"
+
+@app.route("/crearUsuar")
+def testCrearif():
+    nombre = "Juan"
+    email = "juan@etec.uba.ar"
+    if nombre == "Juan":
+        AbrirConexion()
+        cursor = db.cursor()
+        consulta = "INSERT INTO usuarios(usuario, email) VALUES (?, ?);"
+        cursor.execute(consulta, (nombre, email))
+        db.commit()
+        CerrarConexion()  
+        return f"Registro agregado ({nombre})"
+    else:
+        return "Ya esta agregado el usuario"
+
+
 @app.route("/necesito/papel")
 def papel():
     return """<p>Me olvide el papel del baño Ayuda</p>
-    <br>
-    <a href=">necesito/papel">papel</a>
-    <a href=">papel">papel 2</a>
     """
 
 @app.route("/necesito/salir")
 def salir():
     return "<p>Me olvide la llave del baño Ayuda</p>"
 
-@app.route("/nota/DiseñoDeSoftware/<string:nombre>")
+@app.route("/nota/DiseñoDeSoftware/<string:nombr>")
 def notas(nombre):
     n = nombre
     if nombre == "Gabriel":
